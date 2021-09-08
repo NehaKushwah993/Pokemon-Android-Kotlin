b43c288abd9d5aa95a2af550aa19e7e1cebc0040
Neha <neha.kushwah@ignitesol.com>
2021-09-08 22:18:06 +0530

    (TestCase) Testcase for ApiResultTest

---
+++ app/src/test/java/com/nehak/pokemonlist/backend/network/ApiResultTest.kt
@@ -0,0 +1,30 @@
+package com.nehak.pokemonlist.backend.network
+
+import com.nehak.pokemonlist.backend.other.ApiResult
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+
+/**
+ * Created by Neha Kushwah on 8/9/21.
+ */
+@RunWith(JUnit4::class)
+class ApiResultTest {
+
+    @Test
+    fun exception() {
+        val apiResponse = ApiResult.error<String>("error", null);
+        MatcherAssert.assertThat(apiResponse.message, CoreMatchers.`is`("error"))
+    }
+
+    @Test
+    fun success() {
+        val apiResponse = ApiResult.success("data")
+        if (apiResponse.status == ApiResult.Status.SUCCESS) {
+            MatcherAssert.assertThat(apiResponse.data, CoreMatchers.`is`("data"))
+        }
+    }
+}
